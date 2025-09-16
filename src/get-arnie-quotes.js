e033e94c4fcdea8307be019dffff468d7a2bfca5
Donn Zhao <dongyang.tcm@gmail.com>
2025-09-16 21:31:23 +1000

    feat: implement getArnieQuotes using Promise.all as pre requirements

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // Create array of promises for each URL
+  const httpGetPromises = urls.map((url) => httpGet(url));
+
+  // Wait for all promises to resolve
+  const responses = await Promise.all(httpGetPromises);
+
+  const results = responses.map((response) =>
+    response.status === 200
+      ? // Parse the response body and add the result
+        { 'Arnie Quote': JSON.parse(response.body).message }
+      : { FAILURE: JSON.parse(response.body).message }
+  );
+
+  return results;
 };
 
 module.exports = {
